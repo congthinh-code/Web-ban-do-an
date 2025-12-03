a4f4353d134d50010d9640234d76fcb3460e7471
Kaizen <126488715+cxkaizen@users.noreply.github.com>
2025-12-03 13:33:27 +0700

    Delete admin directory

--- admin/database/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host='localhost';
-$db='ten_database_cua_ban';
-$user='root';
-$pass='';
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error) die("Kết nối thất bại: ".$conn->connect_error);
-$conn->set_charset("utf8mb4");
-?>
