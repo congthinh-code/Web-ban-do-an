fbd683e10c7ab2120147218e73e9b2dbd6fe0856
Kaizen <126488715+cxkaizen@users.noreply.github.com>
2025-12-03 13:17:16 +0700

    Delete admin/includes directory

--- admin/includes/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="admin-sidebar">
-    <ul>
-        <li><a href="index.php">Dashboard</a></li>
-        <li><a href="monan.php">Quản lý món ăn</a></li>
-        <li><a href="donhang.php">Quản lý đơn hàng</a></li>
-        <li><a href="nguoidung.php">Quản lý người dùng</a></li>
-        <li><a href="binhluan.php">Quản lý bình luận</a></li>
-    </ul>
-</div>
