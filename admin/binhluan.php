d7e9ff688739ac06194bcf094f01a22d24758028
Kaizen <126488715+cxkaizen@users.noreply.github.com>
2025-12-03 13:16:27 +0700

    Update binhluan.php

--- admin/binhluan.php
+++ admin/binhluan.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../database/db.php';
 $sql = "
     SELECT Binhluan.*, Users.Hoten, Monan.Tenmon
     FROM Binhluan
-    LEFT JOIN Users ON Binhluan.MaKH = Users.MaKH
+    LEFT JOIN Users ON Binhluan.UID = Users.UID
     LEFT JOIN Monan ON Binhluan.Mamon = Monan.Mamon
     ORDER BY MaBL DESC
 ";
@@ -16,11 +16,11 @@ $bl = $conn->query($sql);
 <html>
 <head>
     <title>Quản lý bình luận</title>
-    <link rel="stylesheet" href="assets/admin.css">
+    <link rel="stylesheet" href="../assets/css/admin.css">
 </head>
 <body>
 
-<?php include 'includes/sidebar.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/sidebar.php'; ?>
 
 <main class="admin-main">
     <h2>Quản lý bình luận</h2>
